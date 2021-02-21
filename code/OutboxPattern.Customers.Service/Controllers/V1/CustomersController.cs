277b869ceaf9b1689914352f85935174ada713c5
Adam Smith <adam@rxindustries.io>
2021-02-21 23:01:33 +0000

    Baseline tests for adding a customer in place. No storage in place yet.

--- code/OutboxPattern.Customers.Service/Controllers/V1/CustomersController.cs
+++ code/OutboxPattern.Customers.Service/Controllers/V1/CustomersController.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using OutboxPattern.Customers.Service.Model;
 
@@ -7,8 +8,8 @@ namespace OutboxPattern.Customers.Service.Controllers.V1
     [Route("/api/v1/{controller}")]
     public class CustomersController : Controller
     {
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("{customerId:Guid}")]
+        public IActionResult Get(Guid customerId)
         {
             return Ok();
         }
@@ -16,7 +17,14 @@ namespace OutboxPattern.Customers.Service.Controllers.V1
         [HttpPost]
         public IActionResult Post(AddCustomer addCustomer)
         {
-            return Ok();
+            var customerId = Guid.NewGuid();
+
+            return CreatedAtAction("Get", new {customerId}, new Customer
+                                                            {
+                                                                Id = customerId,
+                                                                FirstName = addCustomer.FirstName,
+                                                                LastName = addCustomer.LastName
+                                                            });
         }
     }
 }
--- code/OutboxPattern.Customers.Tests.Integration/Customers/AddingCustomers.cs
+++ code/OutboxPattern.Customers.Tests.Integration/Customers/AddingCustomers.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -21,75 +22,145 @@ namespace OutboxPattern.Customers.Tests.Integration.Customers
         }
 
         [Scenario]
-        public void CustomersFirstNameNotPopulatedResultsInValidationError(AddCustomer addCustomer, HttpResponseMessage httpResponseMessage)
-        {
-            "If the customers FirstName property is not populated"
-                .x(_ =>
-                   {
-                       addCustomer = new AddCustomer
-                                     {
-                                         FirstName = string.Empty,
-                                         LastName = "Test"
-                                     };
-                   });
-            "When the Customer is posted to the Customers Controller"
-                .x(async _ =>
-                   {
-                       httpResponseMessage = await _webApplicationFactory.CreateClient()
-                                                                         .PostAsJsonAsync("/api/v1/customers",
-                                                                                          addCustomer);
-                   });
-
-            "Then a Bed Request response is sent back with a descriptive error message"
-                .x(async _ =>
-                   {
-                       httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
-                       var responseContent = await httpResponseMessage.Content.ReadFromJsonAsync<Http400StatusResponseModel>();
-                       responseContent.ShouldNotBeNull();
-                       responseContent.Title.ShouldBe("One or more validation errors occurred.");
-                       responseContent.Errors.ShouldNotBeNull();
-                       responseContent.Errors.ShouldContainKey("FirstName");
-                       string[] fieldValidationErrors = responseContent.Errors["FirstName"];
-                       fieldValidationErrors.ShouldNotBeNull();
-                       fieldValidationErrors.Length.ShouldBe(1);
-                       fieldValidationErrors[0].ShouldBe("The FirstName field is required.");
-                   });
-        }
-
-        [Scenario]
         public void CustomersLastNameNotPopulatedResultsInValidationError(AddCustomer addCustomer, HttpResponseMessage httpResponseMessage)
         {
             "If the customers FirstName property is not populated"
-                .x(_ =>
-                   {
-                       addCustomer = new AddCustomer
-                                     {
-                                         FirstName = "Test",
-                                         LastName = string.Empty
-                                     };
-                   });
+               .x(stepContext =>
+                  {
+                      addCustomer = new AddCustomer
+                                    {
+                                        FirstName = "TestFirstName",
+                                        LastName = string.Empty
+                                    };
+                  });
+            
             "When the Customer is posted to the Customers Controller"
-                .x(async _ =>
-                   {
-                       httpResponseMessage = await _webApplicationFactory.CreateClient()
-                                                                         .PostAsJsonAsync("/api/v1/customers",
-                                                                                          addCustomer);
-                   });
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage = await _webApplicationFactory.CreateClient()
+                                                                        .PostAsJsonAsync("/api/v1/customers",
+                                                                                         addCustomer);
+                  });
 
             "Then a Bed Request response is sent back with a descriptive error message"
-                .x(async _ =>
-                   {
-                       httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
-                       var responseContent = await httpResponseMessage.Content.ReadFromJsonAsync<Http400StatusResponseModel>();
-                       responseContent.ShouldNotBeNull();
-                       responseContent.Title.ShouldBe("One or more validation errors occurred.");
-                       responseContent.Errors.ShouldNotBeNull();
-                       responseContent.Errors.ShouldContainKey("LastName");
-                       string[] fieldValidationErrors = responseContent.Errors["LastName"];
-                       fieldValidationErrors.ShouldNotBeNull();
-                       fieldValidationErrors.Length.ShouldBe(1);
-                       fieldValidationErrors[0].ShouldBe("The LastName field is required.");
-                   });
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+                      var responseContent = await httpResponseMessage.Content.ReadFromJsonAsync<Http400StatusResponseModel>();
+                      responseContent.ShouldNotBeNull();
+                      responseContent.Title.ShouldBe("One or more validation errors occurred.");
+                      responseContent.Errors.ShouldNotBeNull();
+                      responseContent.Errors.ShouldContainKey("LastName");
+                      string[] fieldValidationErrors = responseContent.Errors["LastName"];
+                      fieldValidationErrors.ShouldNotBeNull();
+                      fieldValidationErrors.Length.ShouldBe(1);
+                      fieldValidationErrors[0].ShouldBe("The LastName field is required.");
+                  });
+        }
+
+        [Scenario]
+        public void CustomersFirstNameNotPopulatedResultsInValidationError(AddCustomer addCustomer, HttpResponseMessage httpResponseMessage)
+        {
+            "If the customers FirstName property is not populated"
+               .x(stepContext =>
+                  {
+                      addCustomer = new AddCustomer
+                                    {
+                                        FirstName = string.Empty,
+                                        LastName = "TestLastName"
+                                    };
+                  });
+            "When the Customer is posted to the Customers Controller"
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage = await _webApplicationFactory.CreateClient()
+                                                                        .PostAsJsonAsync("/api/v1/customers",
+                                                                                         addCustomer);
+                  });
+
+            "Then a Bed Request response is sent back with a descriptive error message"
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+                      var responseContent = await httpResponseMessage.Content.ReadFromJsonAsync<Http400StatusResponseModel>();
+                      responseContent.ShouldNotBeNull();
+                      responseContent.Title.ShouldBe("One or more validation errors occurred.");
+                      responseContent.Errors.ShouldNotBeNull();
+                      responseContent.Errors.ShouldContainKey("FirstName");
+                      string[] fieldValidationErrors = responseContent.Errors["FirstName"];
+                      fieldValidationErrors.ShouldNotBeNull();
+                      fieldValidationErrors.Length.ShouldBe(1);
+                      fieldValidationErrors[0].ShouldBe("The FirstName field is required.");
+                  });
+        }
+
+        [Scenario]
+        public void CustomerWithValidInputsReturnsCorrectlyFormattedLocationHeader(AddCustomer addCustomer, HttpResponseMessage httpResponseMessage)
+        {
+            "If the customers properties are populated correctly"
+               .x(stepContext =>
+                  {
+                      addCustomer = new AddCustomer
+                                    {
+                                        FirstName = "TestFirstName",
+                                        LastName = "TestLastName"
+                                    };
+                  });
+
+            "When the Customer is posted to the Customers Controller"
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage = await _webApplicationFactory.CreateClient()
+                                                                        .PostAsJsonAsync("/api/v1/customers",
+                                                                                         addCustomer);
+                  });
+
+            "Then the response contains a correctly formatted location header."
+               .x(stepContext =>
+                  {
+                      var expectedUriBase = "http://localhost/api/v1/customers/";
+
+                      httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.Created);
+                      httpResponseMessage.Headers.Location.AbsoluteUri.ShouldStartWith(expectedUriBase);
+                      string idSegment = httpResponseMessage.Headers.Location.Segments.Last();
+                      Guid.TryParse(idSegment, out _).ShouldBeTrue($"The final segment of the URI could not be parsed to a GUID. This should be the customerId. The value was {idSegment}.");
+                      var expectedAbsoluteUrl = $"{expectedUriBase}{idSegment}";
+                      httpResponseMessage.Headers.Location.AbsoluteUri.ShouldBe(expectedAbsoluteUrl, StringCompareShould.IgnoreCase);
+                  });
+        }
+
+
+        [Scenario]
+        public void CustomerWithValidInputsReturnsFullyPopulatedCustomerObject(AddCustomer addCustomer, HttpResponseMessage httpResponseMessage)
+        {
+            "If the customers properties are populated correctly"
+               .x(stepContext =>
+                  {
+                      addCustomer = new AddCustomer
+                                    {
+                                        FirstName = "TestFirstName",
+                                        LastName = "TestLastName"
+                                    };
+                  });
+
+            "When the Customer is posted to the Customers Controller"
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage = await _webApplicationFactory.CreateClient()
+                                                                        .PostAsJsonAsync("/api/v1/customers",
+                                                                                         addCustomer);
+                  });
+
+            "Then the response contains a correctly formatted location header."
+               .x(async stepContext =>
+                  {
+                      httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.Created);
+                      var customer = await httpResponseMessage.Content.ReadFromJsonAsync<Customer>();
+                      customer.ShouldNotBeNull();
+                      customer.Id.ShouldNotBe(Guid.Empty);
+                      customer.FirstName.ShouldBe(addCustomer.FirstName);
+                      customer.LastName.ShouldBe(addCustomer.LastName);
+                  });
         }
     }
 }
